0ccc1a5a8304a7a0d23c52115d49e61e7e9f95a4
CAndRyan <CAndRyan@users.noreply.github.com>
2022-08-03 17:44:01 -0400

    Add initial fourier transform code

---
+++ MTBjorn.CrossStitch.Business/Helpers/Maths/Complex.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MTBjorn.CrossStitch.Business.Helpers.Maths
+{
+	class complex
+	{
+		public float real = 0.0;
+		public float imag = 0.0;
+		//Empty constructor
+		public complex()
+		{
+		}
+		public complex(float real, float im)
+		{
+			this.real = real;
+			this.imag = imag;
+		}
+		public string ToString()
+		{
+			string data = real.ToString() + " " + imag.ToString() + "i";
+			return data;
+		}
+		//Convert from polar to rectangular
+		public static complex from_polar(double r, double radians)
+		{
+			complex data = new complex(r * Math.Cos(radians), r * Math.Sin(radians));
+			return data;
+		}
+		//Override addition operator
+		public static complex operator +(complex a, complex b)
+		{
+			complex data = new complex(a.real + b.real, a.imag + b.imag);
+			return data;
+		}
+		//Override subtraction operator
+		public static complex operator -(complex a, complex b)
+		{
+			complex data = new complex(a.real - b.real, a.imag - b.imag);
+			return data;
+		}
+		//Override multiplication operator
+		public static complex operator *(complex a, complex b)
+		{
+			complex data = new complex((a.real * b.real) - (a.imag * b.imag),
+			(a.real * b.imag + (a.imag * b.real));
+			return data;
+		}
+		//Return magnitude of complex number
+		public float magnitude {
+			get {
+				return Math.Sqrt(Math.Pow(real, 2) + Math.Pow(imag, 2));
+			}
+		}
+		public float phase {
+			get {
+				return Math.Atan(imag / real);
+			}
+		}
+	}
+}
---
+++ MTBjorn.CrossStitch.Business/Helpers/Maths/Fourier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MTBjorn.CrossStitch.Business.Helpers.Maths
+{
+	// Adapted from: https://www.egr.msu.edu/classes/ece480/capstone/fall11/group06/style/Application_Note_ChrisOakley.pdf
+	class fourier
+	{
+		public static complex[] DFT(complex[] x)
+		{
+			int N = x.Length;
+			complex[] X = new complex[N];
+			for (int k = 0; k < N; k++)
+			{
+				X[k] = new complex(0, 0);
+				for (int n = 0; n < N; n++)
+				{
+					complex temp = complex.from_polar(1, -2 * Math.PI * n * k / N);
+					temp *= x[n];
+					X[k] += temp;
+				}
+			}
+			return X;
+		}
+		public static complex[] FFT(complex[] x)
+		{
+			int N = x.Length;
+			complex[] X = new complex[N];
+			complex[] d, D, e, E;
+			if (N == 1)
+			{
+				X[0] = x[0];
+				return X;
+			}
+			int k;
+			e = new complex[N / 2];
+			d = new complex[N / 2];
+			for (k = 0; k < N / 2; k++)
+			{
+				e[k] = x[2 * k];
+				d[k] = x[2 * k + 1];
+			}
+			D = FFT(d);
+			E = FFT(e);
+			for (k = 0; k < N / 2; k++)
+			{
+				complex temp = complex.from_polar(1, -2 * Math.PI * k / N);
+				D[k] *= temp;
+			}
+			for (k = 0; k < N / 2; k++)
+			{
+				X[k] = E[k] + D[k];
+				X[k + N / 2] = E[k] - D[k];
+			}
+			return X;
+		}
+	}
+}
